15434d34530f18116eff1fc1ac2f191da23f5363
master <Welty898@gmail.com>
2025-11-22 12:18:03 +0800

    Lease Term Controller

--- web/web-admin/src/main/java/com/atguigu/lease/web/admin/service/PaymentTypeService.java
+++ web/web-admin/src/main/java/com/atguigu/lease/web/admin/service/PaymentTypeService.java
@@ -3,6 +3,8 @@ package com.atguigu.lease.web.admin.service;
 import com.atguigu.lease.model.entity.PaymentType;
 import com.baomidou.mybatisplus.extension.service.IService;
 
+import java.util.List;
+
 /**
 * @author liubo
 * @description 针对表【payment_type(支付方式表)】的数据库操作Service
